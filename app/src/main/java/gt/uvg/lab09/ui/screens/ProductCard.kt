3f7024081d127e781fee7bc2acc6d82d86e896c4
miguel13sg <saj252149@uvg.edu.gt>
2026-09-15 11:26:55 -0600

    se añadio el catalog con 2 columnas para comparar las composiciones

---
+++ app/src/main/java/gt/uvg/lab09/ui/screens/ProductCard.kt
@@ -0,0 +1,102 @@
+package gt.uvg.lab09.ui.screens
+
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import gt.uvg.lab09.model.Product
+
+@Composable
+fun ProductCard(
+    product: Product,
+    isFavorite: Boolean,
+    onProductClick: (Int) -> Unit,
+    onFavoriteClick: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    //Para catlog
+    DisposableEffect(product.id) {
+        Log.d(
+            "CatalogProbe",
+            "ENTER id=${product.id}"
+        )
+
+        onDispose {
+            Log.d(
+                "CatalogProbe",
+                "EXIT id=${product.id}"
+            )
+        }
+    }
+
+    Card(
+        modifier = modifier
+            .clickable {
+                onProductClick(product.id)
+            }
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            // En la parte de Coil será reemplazado por ProductImage.
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Imagen del producto",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+
+            Text(
+                text = product.name,
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Text(
+                text = "Q %.2f".format(product.price),
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            Text(
+                text = if (product.stock > 0) {
+                    "${product.stock} disponibles"
+                } else {
+                    "Agotado"
+                },
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            TextButton(
+                onClick = {
+                    onFavoriteClick(product.id)
+                }
+            ) {
+                Text(
+                    if (isFavorite) {
+                        "Favorito"
+                    } else {
+                        "Agregar a favoritos"
+                    }
+                )
+            }
+        }
+    }
+}
