ac7de4b767407da563ca511a6665cb682aa23cc7
miguel13sg <saj252149@uvg.edu.gt>
2026-09-15 11:35:13 -0600

    se reemplazó el catalog con lazy grid y se hicieron pruebas

--- app/src/main/java/gt/uvg/lab09/ui/screens/CatalogScreen.kt
+++ app/src/main/java/gt/uvg/lab09/ui/screens/CatalogScreen.kt
@@ -1,14 +1,11 @@
 package gt.uvg.lab09.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -36,57 +33,33 @@ fun CatalogScreen(
         }
     ) { innerPadding ->
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp)
-                .verticalScroll(rememberScrollState()),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(
+                start = 16.dp,
+                end = 16.dp,
+                top = innerPadding.calculateTopPadding() + 16.dp,
+                bottom = innerPadding.calculateBottomPadding() + 16.dp
+            ),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
         ) {
 
-            Text(
-                text = "${products.size} productos"
-            )
-
-            for (i in products.indices step 2) {
-
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(12.dp)
-                ) {
-
-                    Box(
-                        modifier = Modifier.fillMaxWidth(0.5f)
-                    ) {
-                        val product = products[i]
-
-                        ProductCard(
-                            product = product,
-                            isFavorite = product.id in favoriteIds,
-                            onProductClick = onProductClick,
-                            onFavoriteClick = onFavoriteClick,
-                            modifier = Modifier.fillMaxWidth()
-                        )
-                    }
-
-                    if (i + 1 < products.size) {
-
-                        Box(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            val product = products[i + 1]
-
-                            ProductCard(
-                                product = product,
-                                isFavorite = product.id in favoriteIds,
-                                onProductClick = onProductClick,
-                                onFavoriteClick = onFavoriteClick,
-                                modifier = Modifier.fillMaxWidth()
-                            )
-                        }
-                    }
+            items(
+                items = products,
+                key = { product ->
+                    product.id
                 }
+            ) { product ->
+
+                ProductCard(
+                    product = product,
+                    isFavorite = product.id in favoriteIds,
+                    onProductClick = onProductClick,
+                    onFavoriteClick = onFavoriteClick,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
